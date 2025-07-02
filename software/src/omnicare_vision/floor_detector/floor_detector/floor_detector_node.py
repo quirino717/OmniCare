91decabc9289a7589f70ecd0f031bce3aafb42ce
Thiago M <rpj134@gmail.com>
2025-07-02 02:11:41 -0300

    Update floor detector to use 2 models (not finished)

--- software/src/omnicare_vision/floor_detector/floor_detector/floor_detector_node.py
+++ software/src/omnicare_vision/floor_detector/floor_detector/floor_detector_node.py
@@ -14,6 +14,7 @@ from omnicare_msgs.srv import OnOffNode
 
 from ament_index_python.packages import get_package_share_directory
 import os
+from collections import deque, Counter
 from ultralytics import YOLO
 import cv2
 from cv_bridge import CvBridge
@@ -24,7 +25,7 @@ class FloorDetector(Node):
         self.srv = self.create_service(OnOffNode,
                                        "floor_detector/OnOffNode",
                                        self.on_off_node_callbalck)
-        self.__nodeActivate = False
+        self.__nodeActivate = True
 
         self.cam_subscriber = None
         self.image_raw = None
@@ -34,12 +35,19 @@ class FloorDetector(Node):
         self.floor_publisher = self.create_publisher(Int8,
                                                      "floor_detector/atual_floor",
                                                      SensorDataQoS)
+        
+        self.display_publisher = self.create_publisher(Image,
+                                                       "floor_detector/elevator_display",
+                                                       SensorDataQoS)
+        
 
-        self.declare_parameter("model", f"{get_package_share_directory('floor_detector')}/weights/yolo11n.pt")
-        self.model = YOLO(self.get_parameter("model").get_parameter_value().string_value)
-        self.value_classes = self.get_classes() 
-        self.get_logger().info(f"CLASSES DO MODELO: {self.value_classes}")
-        print(self.get_parameter("model").get_parameter_value().string_value)
+        self.declare_parameter("model_display", f"{get_package_share_directory('floor_detector')}/weights/best_display.pt")
+        self.model_display = YOLO(self.get_parameter("model_display").get_parameter_value().string_value)
+        
+        self.declare_parameter("model_floor", f"{get_package_share_directory('floor_detector')}/weights/best_floor.pt")
+        self.model_floor = YOLO(self.get_parameter("model_display").get_parameter_value().string_value)
+
+        self.last_detections = deque(maxlen=10)
 
     def on_off_node_callbalck(self, request, response):
         self.get_logger().info(f"Incoming request\nActivate: {request.activate}")
@@ -64,24 +72,47 @@ class FloorDetector(Node):
     
     def cam_subscriber_callback(self, msg):
         self.image_raw = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-        results = self.model(self.image_raw, verbose=False)[0]
-        annotated_frame = results.plot()
-        self.detect_floor(results)
+        self.detect_floor(self.image_raw.copy())
+
+    # TODO: Terminar        
+    def detect_floor(self, img):
+        floor_msg = Int8()
+        
+        display_results = self.model_display(self.image_raw, verbose=False)[0]
+        annotated_frame = display_results.plot()
         cv2.imshow("YOLO11 Tracking", annotated_frame)
         cv2.waitKey(1)
 
-    def detect_floor(self, results):
-        floor_msg = Int8()
-        if len(results.boxes) == 0:
+        if len(display_results.boxes) == 0:
             return
-        floor_msg.data = int(results.boxes[0].cls)
-        self.floor_publisher.publish(floor_msg)
+        
+        display_img = self.find_display(img, display_results)
+        self.display_publisher.publish(self.bridge.cv2_to_imgmsg(display_img))
+        
+        floor_results = self.model_floor(display_img, vebose=False)[0]
+        floor_filtered = self.filter_floor_detection(floor_results)
+        
+        # floor_msg.data = int(results.boxes[0].cls)
+        # self.floor_publisher.publish(floor_msg)
 
-
-    def get_classes(self): 
-        classes = self.model.names
-        value_classes = {value: key for key, value in classes.items()}
-        return value_classes
+    def find_display(self, img, results):
+        
+        if len(results) == 0:
+            return
+        _ = results.boxes[0].xyxy.tolist()[0] # Get xyxy position of BoundingBox
+        int_xyxy = [int(float(x)) for x in _]
+        return img[int_xyxy[1]:int_xyxy[3], int_xyxy[0]:int_xyxy[2]].copy() 
+    
+    # TODO: terminar essa funcao
+    def filter_floor_detection(self, results):
+        floor = self.model_floor.names[results.boxes[0].cls]
+        print(floor)
+        
+        # self.last_detections.append(floor)
+        # last_detections_counter = Counter(self.last_detections)
+        # mode = last_detections_counter(1)[0][0] # Get the mode of last detections
+        
+        # return mode
     
 
 def main(args=None):
