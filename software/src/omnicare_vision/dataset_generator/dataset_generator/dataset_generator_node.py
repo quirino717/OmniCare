c348370287a1bd9e1697035c692f5655966140dd
Thiago M <rpj134@gmail.com>
2025-08-05 21:45:46 -0300

    Add dataset generator config file && Update dataset config file to save image to a dir with fixed fps

--- software/src/omnicare_vision/dataset_generator/dataset_generator/dataset_generator_node.py
+++ software/src/omnicare_vision/dataset_generator/dataset_generator/dataset_generator_node.py
@@ -13,20 +13,37 @@ from sensor_msgs.msg import Image
 
 from .get_parameters import Parameters
 
+import cv2
+from cv_bridge import CvBridge
+
 class DatasetGenerator(Node):
     def __init__(self):
         super().__init__('dataset_generator')
         self.params = Parameters(self)
+        self.bridge = CvBridge()
 
         self.img_subscriber = self.create_subscription(Image, 
                                                        self.params.img_topic_name,
                                                        self.img_subscriber_callback,
-                                                       SensorDataQoS)        
+                                                       SensorDataQoS)
 
+        self.frames_count = 0
+        self.period_s = 1/self.params.output_frame_fps
+        self.last_time_save_frame = 0
     
     def img_subscriber_callback(self, msg):
-        self.get_logger().info(f'{msg.header}')
         
+        timestamp = msg.header.stamp.sec + msg.header.stamp.nanosec * 1e-9
+        self.get_logger().debug(f'Recieved img time:{timestamp}')
+        
+        if timestamp - self.last_time_save_frame > self.period_s:
+            self.last_time_save_frame = timestamp
+
+            filename = self.params.output_prefix_filename + f'{self.frames_count:03d}.png'
+            cv2.imwrite(self.params.output_dir_path + filename, self.bridge.imgmsg_to_cv2(msg, "bgr8"))
+            self.get_logger().debug(f'Saving {self.params.output_dir_path + filename}')
+            
+            self.frames_count = self.frames_count + 1        
 
 def main(args=None):
     rclpy.init(args=args)
--- software/src/omnicare_vision/dataset_generator/dataset_generator/get_parameters.py
+++ software/src/omnicare_vision/dataset_generator/dataset_generator/get_parameters.py
@@ -6,5 +6,11 @@ class Parameters:
         node.declare_parameter('img_topic_name', 'camera1/image_raw')
         self.img_topic_name = node.get_parameter('img_topic_name').get_parameter_value().string_value
 
-        node.declare_parameter('fps_to_save', 3)
-        self.fps_to_save = node.get_parameter('fps_to_save').get_parameter_value().integer_value
+        node.declare_parameter('output_frame_fps', 3)
+        self.output_frame_fps = node.get_parameter('output_frame_fps').get_parameter_value().integer_value
+
+        node.declare_parameter('output_prefix_filename', 'frame')
+        self.output_prefix_filename = node.get_parameter('output_prefix_filename').get_parameter_value().string_value
+
+        node.declare_parameter('output_dir_path', 'frame')
+        self.output_dir_path = node.get_parameter('output_dir_path').get_parameter_value().string_value
